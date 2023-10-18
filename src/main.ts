18c53486ae52c45250ce000364279e437982b789
alexmarinon <alexmarinon@gmail.com>
2023-10-18 14:46:46 +1100

    readme change

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { quickSort } from "./sort/index.js"
+import { quickSort } from "./sort/index.js";
 
 function generateRandomArray(length: number): number[] {
-    const numberList: number[] = []
+    const numberList: number[] = [];
 
-    for (let i: number = 0; i < length; i++) {
-        numberList[i] = Math.random() * 100
+    for (let i = 0; i < length; i++) {
+        numberList[i] = Math.random() * 100;
     }
 
-    return numberList
+    return numberList;
 }
 
-const generatedArray = generateRandomArray(10)
+const generatedArray = generateRandomArray(10);
 
-console.log(generatedArray)
+console.log(generatedArray);
 
-const sortedArray = quickSort(generatedArray)
+const sortedArray = quickSort(generatedArray);
 
-console.log(sortedArray)
+console.log(sortedArray);
--- src/sort/index.ts
+++ src/sort/index.ts
@@ -1,7 +1,7 @@
 function insertionSort(array: number[]): number[] {
     for (let i = 1; i < array.length; i++) {
-        let currentElement = array[i];
-        let lastIndex = i - 1
+        const currentElement = array[i];
+        let lastIndex = i - 1;
 
         while (lastIndex >= 0 && array[lastIndex] > currentElement) {
             array[lastIndex + 1] = array[lastIndex];
@@ -10,23 +10,21 @@ function insertionSort(array: number[]): number[] {
         array[lastIndex + 1] = currentElement;
     }
 
-    return array
+    return array;
 }
 
 function bubbleSort(array: number[]): number[] {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < (array.length - i - 1); j++) {
-
             if (array[j] > array[j + 1]) {
-                let temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
-
         }
     }
 
-    return array
+    return array;
 }
 
 function quickSort(array: number[]): number[] {
@@ -34,16 +32,15 @@ function quickSort(array: number[]): number[] {
         return array;
     }
 
-    var pivot: number = array[0];
+    const pivot: number = array[0];
+    const left: number[] = [];
+    const right: number[] = [];
 
-    var left: number[] = []; 
-    var right: number[] = [];
-
-    for (var i = 1; i < array.length; i++) {
+    for (let i = 1; i < array.length; i++) {
         array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
     }
 
     return quickSort(left).concat(pivot, quickSort(right));
 }
 
-export { quickSort, bubbleSort, insertionSort }
+export { quickSort, bubbleSort, insertionSort };
