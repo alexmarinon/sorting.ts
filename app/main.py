a21991292d706c568970f331b76b800c18c7cee4
alexmarinon <alexmarinon@gmail.com>
2023-10-18 23:34:59 +1100

    Replaced React view with matplotlib interop

---
+++ app/main.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import os
+
+def plot_sorting_data(data):
+    array_sizes = [row[0] for row in data]
+    bubble_sort_averages = [row[1] for row in data]
+    insertion_sort_averages = [row[2] for row in data]
+    quick_sort_averages = [row[3] for row in data]
+    theoretical_nlogn = [row[5] for row in data]
+    theoretical_nsquared = [row[6] for row in data]
+
+    plt.figure(figsize=(12, 8))
+
+    plt.scatter(array_sizes, bubble_sort_averages, color='blue', label='Bubble Sort')
+    plt.scatter(array_sizes, insertion_sort_averages, color='red', label='Insertion Sort')
+    plt.scatter(array_sizes, quick_sort_averages, color='green', label='Quick Sort')
+    plt.scatter(array_sizes, theoretical_nlogn, color='purple', label='Theoretical nLogN', marker='x')
+    plt.scatter(array_sizes, theoretical_nsquared, color='orange', label='Theoretical nSquared', marker='x')
+
+    # Fitting a 3rd degree polynomial
+    z_bubble = np.polyfit(array_sizes, bubble_sort_averages, 3)
+    z_insertion = np.polyfit(array_sizes, insertion_sort_averages, 3)
+    z_quick = np.polyfit(array_sizes, quick_sort_averages, 3)
+
+    p_bubble = np.poly1d(z_bubble)
+    p_insertion = np.poly1d(z_insertion)
+    p_quick = np.poly1d(z_quick)
+
+    plt.plot(array_sizes, p_bubble(array_sizes), color='blue', linestyle='dashed')
+    plt.plot(array_sizes, p_insertion(array_sizes), color='red', linestyle='dashed')
+    plt.plot(array_sizes, p_quick(array_sizes), color='green', linestyle='dashed')
+
+    plt.title("Sorting Algorithm Average Run Time (ms)", fontsize=16)
+    plt.xlabel("Array Size", fontsize=14)
+    plt.ylabel("Average Time", fontsize=14)
+    plt.legend()
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+    # Setting axes to linear
+    # Removed plt.yscale("log") and plt.xscale("log")
+
+    plt.tight_layout()  # To ensure labels fit within the figure bounds
+    plt.show()
+
+def get_data_from_csv():
+    data = []
+    with open(os.path.join(os.path.dirname(__file__), '../data/benchmark_results.csv'), 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            data_row = [float(value) if i > 0 else int(value) for i, value in enumerate(row)]
+            data.append(data_row)
+
+    return data
+
+data = get_data_from_csv()
+
+plot_sorting_data(data)
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { benchmarkSortFunctionsWithSteps, saveToCSV } from './benchmark/index.js';
+import { exec } from 'child_process';
 import readline from 'readline';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -14,6 +20,16 @@ rl.question('Length of array: ', (arrayLength) => {
 
         const benchmarkResults = benchmarkSortFunctionsWithSteps(arrayUpperBound, iterationsPerArrayPerFunction);
         saveToCSV(benchmarkResults);
+
+        const pythonScriptPath = path.join(__dirname, '../../app/main.py');
+        exec(`python3 ${pythonScriptPath}`, (error, stdout, err) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${err}`);
+        });
 
         rl.close();
     });
