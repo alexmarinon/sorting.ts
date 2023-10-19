3bda83cf1dffe55eb2154e3cf78bf82c02c99d81
alexmarinon <alexmarinon@gmail.com>
2023-10-19 11:35:24 +1100

    index.html

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
 import { benchmarkSortFunctionsWithSteps, saveToCSV } from './benchmark/index.js';
-import { exec } from 'child_process';
+// import { exec } from 'child_process';
 import readline from 'readline';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import express from 'express';
+import fs from 'fs';
+import open from 'open';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3091;
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.get('/data', (_req, res) => {
+    const csvPath = path.join(__dirname, '..', 'data', 'benchmark_results.csv');
+    console.log(`Trying to read from: ${csvPath}`);
+    const data = fs.readFileSync(csvPath, 'utf-8');
+    res.send(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started`);
+});
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -21,15 +40,17 @@ rl.question('Length of array: ', (arrayLength) => {
         const benchmarkResults = benchmarkSortFunctionsWithSteps(arrayUpperBound, iterationsPerArrayPerFunction);
         saveToCSV(benchmarkResults);
 
-        const pythonScriptPath = path.join(__dirname, '../../app/main.py');
-        exec(`python3 ${pythonScriptPath}`, (error, stdout, err) => {
+        open(`localhost:${PORT}/data`)
+
+        // const pythonScriptPath = path.join(__dirname, '../../app/main.py');
+        /*exec(`python3 ${pythonScriptPath}`, (error, stdout, err) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 return;
             }
             console.log(`stdout: ${stdout}`);
             console.error(`stderr: ${err}`);
-        });
+        });*/
 
         rl.close();
     });
